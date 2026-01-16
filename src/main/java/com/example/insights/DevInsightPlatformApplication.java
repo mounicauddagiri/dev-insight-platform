c4090213174c526ae69b01f092b89dd07c992e18
mounicauddagiri <mounicauddagiri@gmail.com>
2026-01-15 20:38:50 -0500

    Disable datasource auto-configuration during initial backend bootstrap

--- src/main/java/com/example/insights/DevInsightPlatformApplication.java
+++ src/main/java/com/example/insights/DevInsightPlatformApplication.java
@@ -2,8 +2,9 @@ package com.example.insights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class DevInsightPlatformApplication {
 
 	public static void main(String[] args) {
